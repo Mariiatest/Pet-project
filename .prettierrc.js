1d42de67338c7254da5c7d9ae581fef55d63bedc
MariiaYusypovych <mariia.yusypovych@empeek.io>
2025-05-26 11:08:52 +0300

    Test with error

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 1,
+    requireConfig: true,
+    bracketSpacing: true}
---
+++ pages/homepage.js
@@ -0,0 +1,17 @@
+export class Homepage {
+ constructor(page) {}
+ phoneInput = '[inputmode="tel"]'
+ searchFormSubmit = '.search-form__submit'
+ submitPhone = '[data-qaid="submit-phone"]'
+ iconMenu = '[rziconname="icon-user-simple"]'
+ iconHeaderBasket = '[rziconname="icon-header-basket"]'
+ elementOnHomepage = '[class="item"]'
+ loginIcon = '[rziconname="icon-user-simple"]'
+
+
+
+
+
+
+
+}
